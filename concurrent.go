f3687235a978ef61a884954c4ffc546eb86f02fc
RafalyAhmad <136140993+RafalyAhmad@users.noreply.github.com>
2026-05-06 11:16:55 +0700

    Update concurrent.go

--- concurrent.go
+++ concurrent.go
@@ -1,1 +1,11 @@
 // goroutine dan channel
+
+func recursiveFunction(n int) int {
+    // Base case
+    if n == 0 {
+        return 1
+    }
+
+    // Recursive case
+    return n * recursiveFunction(n-1)
+}
